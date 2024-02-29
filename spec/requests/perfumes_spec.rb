c3219e066d89e359eaf3147eb890b49ce96d06a3
chantelcarter <chantel.alexia@gmail.com>
2024-02-28 18:00:20 -0800

    controller method for index and rspec test coverage

--- spec/requests/perfumes_spec.rb
+++ spec/requests/perfumes_spec.rb
@@ -1,7 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe "Perfumes", type: :request do
+  let(:user) { User.create(
+    email: 'test@example.com',
+    password: 'password',
+    password_confirmation: 'password'
+    )
+  }
+
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'gets a list of perfumes' do 
+      perfume = user.perfumes.create(
+        name: "Accident À La Vanille",
+        designer: "Jousset Parfums",
+        category: "Gourmand",
+        top_notes: "Vanilla",
+        middle_notes: "Vanilla, Sandalwood",
+        base_notes: "Vanilla, Styrax",
+        image: "https://fimgs.net/mdimg/perfume/375x500.68076.jpg"    
+      )
+
+      get '/perfumes'
+
+      perfume = JSON.parse(response.body)
+      expect(perfume.first['name']).to eq("Accident À La Vanille")
+      expect(perfume.first['designer']).to eq("Jousset Parfums")
+      expect(perfume.first['category']).to eq("Gourmand")
+      expect(perfume.first['top_notes']).to eq("Vanilla")
+      expect(perfume.first['middle_notes']).to eq("Vanilla, Sandalwood")
+      expect(perfume.first['base_notes']).to eq("Vanilla, Styrax")
+      expect(perfume.first['image']).to eq("https://fimgs.net/mdimg/perfume/375x500.68076.jpg")
+      expect(response).to have_http_status(200)
+    end
   end
 end
