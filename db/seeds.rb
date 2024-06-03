0971e7fc9fc0f9873e81da6ad98598ec8cb09481
Chantel Carter <chantel.alexia@gmail.com>
2024-06-03 09:28:23 -0700

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,87 @@ user1_perfumes = [
     middle_notes: "Vodka, Raspberry, Basil",
     base_notes: "Patchouli, Amber, Olibanum, Musk",
     image: "https://fimgs.net/mdimg/perfume/375x500.75021.jpg"
+  },
+  {
+    name: "Chocolate Greedy",
+    designer: "Montale",
+    category: "Gourmand",
+    top_notes: "Bitter Orange, Dried Fruits",
+    middle_notes: "Cocao, Coffee Bean",
+    base_notes: "Tonka Bean, Vanilla",
+    image: "https://fimgs.net/mdimg/perfume/375x500.2186.jpg"
+  },
+  {
+    name: "Instant Crush",
+    designer: "Mancera",
+    category: "Floral",
+    top_notes: "Saffron, Ginger, Sicilian Mandarin, Sicilian Bergamot",
+    middle_notes: "Amberwood, Moroccan Rose, Egyptian Jasmine, Indonesian Patchouli Leaf",
+    base_notes: "Madagascar Vanilla, White Musk, Sandalwood, Oakmoss",
+    image: "https://fimgs.net/mdimg/perfume/375x500.54885.jpg"
+  },
+  {
+    name: "Blanche",
+    designer: "Byredo",
+    category: "Floral",
+    top_notes: "Aldehydes, Rose, Pink Pepper",
+    middle_notes: "Peony, Violet, African Orange Flower",
+    base_notes: "Musk, Woodsy Notes, Sandalwood",
+    image: "https://fimgs.net/mdimg/perfume/375x500.6686.jpg"
+  },
+  {
+    name: "Poets of Berlin",
+    designer: "Vilhelm Parfumerie",
+    category: "Fruity",
+    top_notes: "Blueberry, Lemon",
+    middle_notes: "Bamboo, Orris",
+    base_notes: "Vanilla, Sandalwood, Vetiver",
+    image: "https://fimgs.net/mdimg/perfume/375x500.49306.jpg"
+  },
+  {
+    name: "Gentle Fluidity Gold",
+    designer: "Maison Francis Kurkdjian",
+    category: "Vanilla",
+    top_notes: "Juniper Berries, Nutmeg, Coriander",
+    middle_notes: "Musk, Vanilla, Amber",
+    base_notes: "Vetiver, Woods, Vanilla",
+    image: "https://fimgs.net/mdimg/perfume/375x500.53401.jpg"
+  },
+  {
+    name: "Buontalenti",
+    designer: "Profumo di Firenze",
+    category: "Vanilla",
+    top_notes: "Banana, Apricot, Peach, Red Fruits",
+    middle_notes: "Coconut, Vanilla, Raspberry",
+    base_notes: "Cookie, Toffee, Caramel, Musk",
+    image: "https://fimgs.net/mdimg/perfume/375x500.57112.jpg"
+  },
+  {
+    name: "Bianco Latte Limited Edition",
+    designer: "Giardini Di Toscana",
+    category: "Gourmand",
+    top_notes: "Caramel",
+    middle_notes: "Coumarin, Honey",
+    base_notes: "Vanilla, White Musk",
+    image: "https://fimgs.net/mdimg/perfume/375x500.88107.jpg"
+  },
+  {
+    name: "Escapade Gourmande",
+    designer: "Maison Mataha",
+    category: "Gourmand",
+    top_notes: "Sugar Cane",
+    middle_notes: "Vanilla, Tonka Bean",
+    base_notes: "Benzoin, White Musk",
+    image: "https://fimgs.net/mdimg/perfume/375x500.72607.jpg"
+  },
+  {
+    name: "Bal d'Afrique",
+    designer: "Byredo",
+    category: "Woody",
+    top_notes: "Almafi Lemon, Tegetes, Black Currant, Bergamot, African Orange Flower",
+    middle_notes: "Violet, Cyclamen, Jasmine",
+    base_notes: "Vetiver, Musk, Amber, Virginia Cedar",
+    image: "https://fimgs.net/mdimg/perfume/375x500.6458.jpg"
   }
 ]
 
